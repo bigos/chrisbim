d50e9b1a5663beef40ce49ead8372ad4eac95317
Jacek Podkanski <ruby.object@googlemail.com>
2012-12-20 22:24:46 +0000

    set up user model

--- db/migrate/20121220220616_create_users.rb
+++ db/migrate/20121220220616_create_users.rb
@@ -3,6 +3,18 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
 
       t.timestamps
+      t.string :login, :null => false
+      t.string :crypted_password, :null => false
+      t.string :password_salt, :null => false
+      t.string :persistence_token, :null => false
+      t.string  :perishable_token,    :null => false
+      t.integer :login_count, :default => 0, :null => false
+      t.integer :failed_login_count, :default => 0, :null => false
+      t.datetime :last_request_at
+      t.datetime :last_login_at
+      t.datetime :current_login_at
+      t.string :last_login_ip
+      t.string :current_login_ip
     end
   end
 end
