64fa95c74a7f6ae0df8dfc6c4c12353f23bd5d51
Jacek Podkanski <ruby.object@googlemail.com>
2012-12-20 15:13:39 +0000

    created new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chrisbim::Application.config.secret_token = '3fa564ec98c97a3d84a07806406d57ae4a75dfb1d32151fa445bd4b56cd359a2f8d066f009ddab9525e009920dbd658d56e88b08031f4613f5bb6cff7daf1343'
