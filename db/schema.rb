7f1300205974749d29a79731e568cf30fb8fbdbf
Jacek Podkanski <ruby.object@googlemail.com>
2012-12-21 00:55:42 +0000

    screated page scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121220232251) do
+ActiveRecord::Schema.define(:version => 20121221005229) do
+
+  create_table "pages", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.string   "image"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "sessions", :force => true do |t|
     t.string   "session_id", :null => false
