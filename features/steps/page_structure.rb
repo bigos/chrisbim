0ca1a65ba8cc1ccd4ac588c8bc257a6248c996e2
Jacek Podkanski <ruby.object@googlemail.com>
2013-01-01 02:33:09 +0000

    found way to programmatically search DOM tree

--- features/steps/page_structure.rb
+++ features/steps/page_structure.rb
@@ -5,6 +5,16 @@ class Spinach::Features::PageStructure < Spinach::FeatureSteps
   end
 
   step 'it should contain required links' do
-    pending 'step not implemented'
+    #pending 'step not implemented'
+    links = page.all(:xpath, '//div[@id="navbar"]/a')
+    puts 'links found in navbar:'
+    links.each do |l|
+      puts 'Link to '+ l.text
+      #p l.value
+      #p l[:title]
+    end
+    puts '###########'
+    puts page.first(:xpath, '//div[@id="navbar"]/a').inspect
+    
   end
 end
