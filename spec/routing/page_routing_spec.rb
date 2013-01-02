b995ab698d3d6063b370207a8ab462ce79f21dce
Jacek Podkanski <ruby.object@googlemail.com>
2013-01-02 19:19:00 +0000

    added test for all routes

--- spec/routing/page_routing_spec.rb
+++ spec/routing/page_routing_spec.rb
@@ -3,8 +3,15 @@ require "spec_helper"
 describe PageController do
   describe "routing" do
 
-    it "routes to #index" do
+    it "routes to #home" do
       get("/home").should route_to("page#home")
+    end
+    it "routes to other pages" do
+      get("/news").should route_to("page#news")
+      pages=['news', 'about_me', 'gallery', 'workshops', 'comments', 'links', 'contact']
+      pages.each do |page|
+        get('/'+page).should route_to('page#'+page)
+      end
     end
   end
 end
