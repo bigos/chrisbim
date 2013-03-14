6077bfe9ba8e6076e0d38678d2f7bc6224f4fa10
Jacek Podkanski <ruby.object@googlemail.com>
2013-03-14 00:14:51 +0000

    created contact message model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121230145321) do
+ActiveRecord::Schema.define(:version => 20130314001351) do
 
   create_table "ckeditor_assets", :force => true do |t|
     t.string   "data_file_name",                  :null => false
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(:version => 20121230145321) do
 
   add_index "ckeditor_assets", ["assetable_type", "assetable_id"], :name => "idx_ckeditor_assetable"
   add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], :name => "idx_ckeditor_assetable_type"
+
+  create_table "contact_messages", :force => true do |t|
+    t.string   "from"
+    t.string   "subject"
+    t.text     "message"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "pages", :force => true do |t|
     t.string   "title"
